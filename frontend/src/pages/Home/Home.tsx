ec5b92f8b4a7c664ae2a400e5c0f193fcd669445
guizzie <guizzie@outlook.com>
2025-02-19 16:32:05 -0300

    feat: criação da página inicial do site

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import { Box } from '@mui/material'; // Importando o Box do Material-UI
+import { Topbar } from '../../components/Topbar/Topbar'; // Importando o componente Topbar
+
+function Home() {
+  return (
+    <Box>
+      {}
+      <Topbar />
+
+      {}
+      <Box />
+    </Box>
+  );
+}
+
+export { Home };
