4f40faf421db56bddf1dfc09f238fc0810f1ed91
guizzie <guizzie@outlook.com>
2025-02-19 16:29:46 -0300

    feat: criação de rotas de algumas páginas novas

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { UserList } from "../pages/User/UserList";
 import { Products } from "../pages/Product/Products";
-
-
+import { DogItems } from "../pages/DogItems/DogItems"; 
+import { Home } from "../pages/Home/Home";
+ 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<UserList />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/user-list" element={<UserList />} />
                 <Route path="/products" element={<Products />} />
+                <Route path="/dog-items" element={<DogItems/>} />
             </Routes>
         </BrowserRouter>
     )
