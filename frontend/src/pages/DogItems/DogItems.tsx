a3805e7881827fac4309e02ec81f37aebf8a8996
guizzie <guizzie@outlook.com>
2025-02-19 16:33:04 -0300

    feat: criação da página de itens de cachorro

---
+++ frontend/src/pages/DogItems/DogItems.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import { Topbar } from "../../components/Topbar/Topbar";
+
+function DogItems() {
+  return (
+    <Box>
+        <Topbar />
+    </Box>
+  );
+}
+
+export { DogItems } 
