a36cdffc783b4b3b796190f575078c6b03aef8eb
guizzie <guizzie@outlook.com>
2025-02-21 14:50:19 -0300

    feat(front): add Cart

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,9 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { UserList } from "../pages/User/UserList";
 import { Products } from "../pages/Product/Products";
-import { DogItems } from "../pages/DogItems/DogItems"; 
 import { Home } from "../pages/Home/Home";
- 
+import { Cart } from "../pages/Cart/Cart"
 const AppRoutes = () => {
     return (
         <BrowserRouter>
@@ -11,7 +10,7 @@ const AppRoutes = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/user-list" element={<UserList />} />
                 <Route path="/products" element={<Products />} />
-                <Route path="/dog-items" element={<DogItems/>} />
+                <Route path="/cart" element={<Cart/>} />
             </Routes>
         </BrowserRouter>
     )
